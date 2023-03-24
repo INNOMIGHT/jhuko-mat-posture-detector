7fa63a51e98298fb6855203e782a9dfa09aa0815
Sujay Paul <58883319+sujaypaul@users.noreply.github.com>
2023-03-24 15:20:52 +0530

    correction

--- jhukomatfrontend/src/components/profile.js
+++ jhukomatfrontend/src/components/profile.js
@@ -43,7 +43,7 @@ const Profile = () => {
     setSelectedFile(file);
   }
   function handleUploadButtonClick() {
-    setPicture(selectedFile);
+    setPicture(URL.createObjectURL(selectedFile));
     savePicture(selectedFile);
   }
 
